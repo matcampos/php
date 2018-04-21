b4a400f1b98a44a3854326b8d87a24b52d5b2d75
logonrm <logonrm@fiap.com.br>
2018-04-20 21:24:39 -0300

    adicionando export do banco

--- CMS/cms.sql
+++ CMS/cms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 14-Abr-2018 às 01:47
+-- Generation Time: 17-Abr-2018 às 03:48
 -- Versão do servidor: 5.7.20-log
 -- PHP Version: 7.1.11
 
@@ -44,6 +44,30 @@ INSERT INTO `categoria` (`cat_id`, `cat_nome`) VALUES
 (4, 'Redes'),
 (5, 'IoT');
 
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `posts`
+--
+
+CREATE TABLE `posts` (
+  `post_id` int(11) NOT NULL,
+  `post_nome` varchar(200) NOT NULL,
+  `post_autor` varchar(100) NOT NULL,
+  `post_data` date NOT NULL,
+  `post_imagem` text NOT NULL,
+  `post_conteudo` text NOT NULL,
+  `post_status` varchar(20) NOT NULL DEFAULT 'draft'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `posts`
+--
+
+INSERT INTO `posts` (`post_id`, `post_nome`, `post_autor`, `post_data`, `post_imagem`, `post_conteudo`, `post_status`) VALUES
+(1, 'Aula de PHP', 'Gustavo Torrente', '2018-04-16', 'cms_post_image1.jpg', 'Esta aula de PHP esta voltada para criação de um sistema de gerenciamento de conteúdo (CMS).', 'draft'),
+(2, 'Google para Empreendedores', 'Glaucio Daniel', '2018-04-17', 'google.png', 'Este post é destinado a ferramentas do Google para empreendedores, veja mais..', 'draft');
+
 --
 -- Indexes for dumped tables
 --
@@ -55,6 +79,12 @@ ALTER TABLE `categoria`
   ADD PRIMARY KEY (`cat_id`);
 
 --
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`post_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -63,6 +93,12 @@ ALTER TABLE `categoria`
 --
 ALTER TABLE `categoria`
   MODIFY `cat_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
