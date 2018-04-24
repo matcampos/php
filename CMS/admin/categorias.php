38a19da5a3bcedb7b0a1d93713beb4797de9fff4
logonrm <logonrm@fiap.com.br>
2018-04-23 22:45:08 -0300

    Adicionando exercicio

--- CMS/admin/categorias.php
+++ CMS/admin/categorias.php
@@ -1,4 +1,4 @@
-
+<?php ob_start(); ?>
 <?php  include 'includes/header.php';   
         include '../includes/db.php';?>
 <div id="wrapper">
@@ -11,12 +11,17 @@
     $select_cat = mysqli_query($connection, $query); 
 
     if(isset($_POST['adicionar'])){
+        if($_POST['cat_nome'] != ""){
         $nome = $_POST['cat_nome'];
         $query2 = "INSERT INTO categoria (cat_nome) VALUES('$nome')";
         $select_cat = mysqli_query($connection, $query2); 
 
         $query = "SELECT * FROM categoria";
         $select_cat = mysqli_query($connection, $query); 
+        }else{
+            echo "<h1 style='color:white;'>Insira um nome!</h1>";
+        }
+
     }
             ?>
 
@@ -43,6 +48,39 @@
                             <input type="submit" onclick="myFunction()" class="btn btn-primary" value="adicionar" name="adicionar">
                         </div>
                     </form>
+                    <form action="categorias.php" method="post">
+                        <div class="form-group">
+                            <label for="cat_nome">
+                                Editar categoria
+                            </label>
+                   
+
+                    <?php 
+                    if(isset($_GET['update'])){
+                        $cat_id = $_GET['update'];
+                            $query = "SELECT * FROM categoria WHERE cat_id = $cat_id";
+                            $select_cat_id = mysqli_query($connection, $query); 
+                            while($row = mysqli_fetch_assoc($select_cat_id)){
+                                    $cat_id = $row['cat_id'];
+                                    $cat_nome = $row['cat_nome'];
+                            
+                    ?>
+
+                    <input value="<?php if(isset($cat_nome)){echo $cat_nome;}?>" type="text" class="form-control" name="cat_nome"/>
+                    
+                            <?php
+
+                        }
+
+                    }
+
+                    ?>
+                      </div>
+                        <div class="form-group">
+                            <input type="submit" onclick="myFunction()" class="btn btn-primary" value="Atualizar" name="update">
+                        </div>
+                      
+                    </form>
                     </div>
 
                     <div class="col-sm-6">
@@ -50,22 +88,38 @@
                                 <thead>
                                     <th>ID</th>
                                     <th>Nome da Categoria</th>
+                                    <th></th>
+                                    <th></th>
                                 </thead>
                         
                             <tbody>
                             <?php
+                            
                          while($row = mysqli_fetch_assoc($select_cat)){
                                 $cat_id = $row['cat_id'];
                                 $cat_nome = $row['cat_nome'];
                                 echo "<tr>";
                                 echo "<td> $cat_id </td>";
                                 echo "<td> $cat_nome </td>";
-                                echo "</tr>";
+                                echo "<td> <a href='categorias.php?delete=$cat_id'>Apagar</a> </td>";
+                                echo "<td> <a href='categorias.php?update=$cat_id'>Editar</a> </td>";
                          }
                  
                                 ?>
                             </tbody>
+
+                            <?php
+                               if(isset($_GET['delete'])){
+                                $id = $_GET['delete'];
+                                $query3 = "DELETE FROM categoria WHERE cat_id = $id";
+                                $delete_cat = mysqli_query($connection, $query3); 
+                                header('location: categorias.php');
+                                
+                                }
+                            ?>
                         </table>
+
+
                     </div>
                 </div>
             </div>
